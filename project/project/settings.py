31ff6dc8709cf16753d01cca66efb5815ab587a6
lee sang hun <oper6909@gmail.com>
2021-08-23 09:29:07 +0900

    ci: hide django secret key

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-os3^+8!&-nwe67v1mpwf@s$b6(q7)h65q5c1c6h_o^=$!c---z"
+# MUST hide this secret key. how will you handle this?
+SECRET_KEY = "django-insecure-_____________________________________"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
